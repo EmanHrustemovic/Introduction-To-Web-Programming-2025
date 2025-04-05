e6015e966782b3ea52599b2d000a329b9b6b3bc0
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-04-06 00:54:28 +0200

    ZdravstveniKartonDao tests

--- backend/dao/ProjectDao.php
+++ backend/dao/ProjectDao.php
@@ -1,4 +1,6 @@
 <?php
+namespace App\dao;
+
 
 require_once __DIR__ . "/../services/config.php";
 
@@ -11,16 +13,16 @@ class ProjectDao {
         $this->table = $table;
 
         try {
-            $this->connection = new PDO(
+            $this->connection = new \PDO(
                 "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT,
                 DB_USER,
                 DB_PASSWORD,
                 [
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
                 ]
             );
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             throw $e;
         }
     }
