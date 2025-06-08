96b4e97b414a4b6554eb07756571851c672ada9b
EmanHrustemovic <eman.hrustemovic@stu.ibu.edu.ba>
2025-06-09 01:08:54 +0200

    DO Config

--- backend/services/config.php
+++ backend/services/config.php
@@ -6,20 +6,38 @@ use PDO;
 use PDOException;
 
 class Database {
-   private static $host = 'localhost';
+   /*
+   private static $host = self::get_env("DB_NAME", "localhost");
    private static $dbName = 'moje_zdravlje_a';
    private static $username = 'root';
    private static $password = 'g3c9h.,1?0';
    private static $connection = null;
+   */
+  public static function DB_NAME() {
+    return Config::get_env("DB_NAME", "university");
+}
+   public static function DB_PORT() {
+       return Config::get_env("DB_PORT", 3306);
+   }
+   public static function DB_USER() {
+       return Config::get_env("DB_USER", 'root');
+   }
+   public static function DB_PASSWORD() {
+       return Config::get_env("DB_PASSWORD", '');
+   }
+   public static function DB_HOST() {
+       return Config::get_env("DB_HOST", '127.0.0.1');
+   }
+
 
 
    public static function connect() {
        if (self::$connection === null) {
            try {
                self::$connection = new PDO(
-                   "mysql:host=" . self::$host . ";dbname=" . self::$dbName,
-                   self::$username,
-                   self::$password,
+                   "mysql:host=" . self::DB_HOST() . ";port=" . self::DB_PORT() . ";dbname=" . self::DB_NAME(),
+                   self::DB_USER(),
+                   self::DB_PASSWORD(),
                    [
                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -31,6 +49,9 @@ class Database {
        }
        return self::$connection;
    }
+   public static function get_env($name, $default){
+    return isset($_ENV[$name]) && trim($_ENV[$name]) != "" ? $_ENV[$name] : $default;
+}
 }
 class Config {
     public static function JWT_SECRET() {
